572147064807078af575ae0792dcf7883a491bb3
serioushyeon <zz64446949@gmail.com>
2025-05-12 02:37:59 +0900

    #1 [setting] : dummy component 추가

---
+++ app/src/main/java/com/sopt/at/uber/core/component/DummyComponent.kt
@@ -0,0 +1,9 @@
+package com.sopt.at.uber.core.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DummyComponent(modifier: Modifier = Modifier) {
+
+}
