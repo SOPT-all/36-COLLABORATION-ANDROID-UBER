74ea789afb0266f904844a7859344341ab36ade0
serioushyeon <zz64446949@gmail.com>
2025-05-12 02:40:09 +0900

    #1 [del] : 불필요 파일 제거

--- app/src/main/java/com/sopt/at/uber/core/util/ContextExt.kt
+++
@@ -1,8 +0,0 @@
-package com.sopt.at.uber.core.util
-
-import android.content.Context
-import android.widget.Toast
-
-fun Context.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-}
