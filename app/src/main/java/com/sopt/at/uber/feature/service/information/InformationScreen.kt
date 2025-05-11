f7e0fcf5aacec064e154ba2859460a19df7e1a8b
serioushyeon <zz64446949@gmail.com>
2025-05-11 17:30:28 +0900

    #1 [setting] : 폴더링 및 기본 스크린 생성

---
+++ app/src/main/java/com/sopt/at/uber/feature/service/information/InformationScreen.kt
@@ -0,0 +1,11 @@
+package com.sopt.at.uber.feature.service.information
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun InformationScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
---
+++ app/src/main/java/com/sopt/at/uber/feature/service/vehicle/VehicleScreen.kt
@@ -0,0 +1,11 @@
+package com.sopt.at.uber.feature.service.vehicle
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun VehicleScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
