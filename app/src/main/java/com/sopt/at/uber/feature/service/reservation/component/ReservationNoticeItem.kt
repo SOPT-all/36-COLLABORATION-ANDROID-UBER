cf223da2f94c73e68255ff48a64c643f649edf8f
serioushyeon <zz64446949@gmail.com>
2025-05-16 14:40:56 +0900

    #11 [del] : 불필요 임포트 제거

--- app/src/main/java/com/sopt/at/uber/feature/service/reservation/component/ReservationNoticeItem.kt
+++ app/src/main/java/com/sopt/at/uber/feature/service/reservation/component/ReservationNoticeItem.kt
@@ -2,11 +2,9 @@ package com.sopt.at.uber.feature.service.reservation.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
